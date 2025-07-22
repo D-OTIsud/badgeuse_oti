a682ec61761f2cfb2f5ffe0196429a1a84a74677
D-OTIsud <d.philippe@otisud.com>
2025-07-22 15:44:11 +0400

    lottie animation

--- src/components/LottieLoader.tsx
+++ src/components/LottieLoader.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
 const LottieLoader: React.FC = () => (
-  <div style={{ width: 120, height: 120, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+  <div
+    style={{
+      width: 120,
+      height: 90, // 120px - 30px crop
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      overflow: 'hidden', // crop
+      position: 'relative'
+    }}
+  >
     <lottie-player
       src="https://cdn.lottielab.com/l/5sVSEnWYQNEv1F.json"
       background="transparent"
       speed="1"
-      style={{ width: '100%', height: '100%' }}
+      style={{
+        width: '100%',
+        height: 120,
+        marginTop: -30 // shift up to crop bottom
+      }}
       loop
       autoplay
     ></lottie-player>
